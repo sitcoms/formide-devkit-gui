fe97982589716045959aa3b139040727117652df
ChrisTerBeke <ctbeke@gmail.com>
2015-05-19 14:53:52 +0200

    add build

--- build.js
+++ build.js
@@ -1,39 +1,29 @@
 // modules
 var execSync = require('child_process').execSync;
 var NwBuilder = require('node-webkit-builder');
+var dependencies = require('./package.json').dependencies;
 
-// flow
-try {
-	execSync("rm -r node_modules");
+var files = [
+	"./app/**",
+    "./public/**",
+    "./core/**",
+    "./formidesdk.icns",
+    "./formidesdk.png",
+    "./LICENSE.md",
+    "./README.MD",
+    "./package.json"
+];
+
+for(var dep in dependencies) {
+	files.push("./node_modules/" + dep + "/**");
 }
-catch(e) {}
-try {
-	execSync("npm install --production");
-}
-catch(e) {}
-try {
-	build(function() {
-		try {
-			execSync("npm install");
-		}
-		catch(e) {}
-	});
-}
-catch(e) {}
+
+build();
 
 // build
-function build(cb) {
+function build() {
 	var nw = new NwBuilder({
-	    files: [
-		    "./app/**",
-		    "./public/**",
-		    "./core/**",
-		    "./formidesdk.icns",
-		    "./formidesdk.png",
-		    "./LICENSE.md",
-		    "./README.MD",
-		    "./package.json"
-	    ],
+	    files: files,
 	    platforms: ['osx32', 'osx64', 'win32', 'win64'],
 	    version: "0.12.1",
 	    buildType: "versioned",
@@ -49,7 +39,6 @@ function build(cb) {
 	// Build returns a promise 
 	nw.build().then(function () {
 	   console.log('all done!');
-	   cb();
 	}).catch(function (error) {
 	    console.error(error);
 	});
