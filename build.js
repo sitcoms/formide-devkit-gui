6990fcd3e1c37262b6be1685b0446b98da7e7dc9
ChrisTerBeke <ctbeke@gmail.com>
2015-05-28 12:55:39 +0200

    custom build script

--- build.js
+++ build.js
@@ -1,45 +1,58 @@
-// modules
-var execSync = require('child_process').execSync;
-var NwBuilder = require('node-webkit-builder');
-var dependencies = require('./package.json').dependencies;
+var fs				= require('fs');
+var path			= require('path');
+var child_process	= require('child_process');
+
+process.chdir( __dirname );
+
+// get dependencies
+var manifest = JSON.parse( fs.readFileSync('package.json').toString() );
 
 var files = [
-	"./app/**",
-    "./public/**",
-    "./core/**",
-    "./formidesdk.icns",
-    "./formidesdk.png",
-    "./LICENSE.md",
-    "./README.MD",
-    "./package.json"
+	'./core/**/**',
+	'./app/**/**',
+	'./package.json',
+	'./public/**/**'
 ];
 
-for(var dep in dependencies) {
-	files.push("./node_modules/" + dep + "/**");
+for( var dependency in manifest.dependencies ) {
+	files.push( './node_modules/' + dependency + '/**/**' );
 }
 
-build();
+//child_process.execSync('sudo ulimit -n 4096');
+//child_process.execSync('rm ./core');
+child_process.execSync('rm -rf ./build');
+//child_process.execSync('cp -r ../devkit-core/core/ core');
 
-// build
-function build() {
-	var nw = new NwBuilder({
-	    files: files,
-	    platforms: ['osx32', 'osx64', 'win32', 'win64'],
-	    version: "0.12.1",
-	    buildType: "versioned",
-	    appVersion: "0.1.0",
-	    appName: "Formide DevKit",
-	    macIcns: "./formidesdk.icns",
-	    winIco: ""
-	});
-	 
-	//Log stuff you want 
-	nw.on('log',  console.log);
-	 
-	// Build returns a promise 
-	nw.build().then(function () {
+var NwBuilder = require('node-webkit-builder');
+var nw = new NwBuilder({
+	
+	files: files,
+	
+	macIcns: './app/assets/icons/osx.icns',
+//	winIco: './app/assets/icons/win.icns',
+	
+    platforms: ['osx64', 'win32', 'win64'],
+    
+    version: '0.12.0'
+    
+});
+
+// log all the things!
+nw.on('log',  console.log);
+
+// build!
+nw
+	.build()
+	.then(function () {
 	   console.log('all done!');
-	}).catch(function (error) {
+	   done();
+	})
+	.catch(function (error) {
 	    console.error(error);
+	    done();
 	});
+	
+function done() {
+	//child_process.execSync('rm -rf core');	
+	//child_process.execSync('ln -s ../devkit-core core');	
 }
